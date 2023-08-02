2218a5ebd59f31aabdb894db7b2822f8f63f3024
Kezhu Wang <kezhuw@gmail.com>
2023-08-03 02:04:46 +0800

    fix: stripe chroot for created path in multi writer

--- tests/zookeeper.rs
+++ tests/zookeeper.rs
@@ -8,6 +8,7 @@ use rand::distributions::Standard;
 use rand::{self, Rng};
 #[allow(unused_imports)]
 use tempfile::{tempdir, TempDir};
+use test_case::test_case;
 use testcontainers::clients::Cli as DockerCli;
 use testcontainers::core::{Healthcheck, RunnableImage, WaitFor};
 use testcontainers::images::generic::GenericImage;
@@ -109,14 +110,39 @@ async fn test_example() {
     tokio::spawn(async move { example().await }).await.unwrap()
 }
 
+async fn connect(cluster: &str, chroot: &str) -> zk::Client {
+    let client = zk::Client::connect(cluster).await.unwrap();
+    if chroot.len() <= 1 {
+        return client;
+    }
+    let create_options = zk::CreateOptions::new(zk::CreateMode::Persistent, zk::Acl::anyone_all());
+    let mut i = 1;
+    while i <= chroot.len() {
+        let j = match chroot[i..].find('/') {
+            Some(j) => j + i,
+            None => chroot.len(),
+        };
+        let path = &chroot[..j];
+        match client.create(path, Default::default(), &create_options).await {
+            Ok(_) | Err(zk::Error::NodeExists) => {},
+            Err(err) => panic!("{err}"),
+        }
+        i = j + 1;
+    }
+    client.chroot(chroot).unwrap()
+}
+
+#[test_case("/"; "no_chroot")]
+#[test_case("/x"; "chroot_x")]
+#[test_case("/x/y"; "chroot_x_y")]
 #[tokio::test]
-async fn test_multi() {
+async fn test_multi(chroot: &str) {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
     let zk_port = zookeeper.get_host_port(2181);
 
     let cluster = format!("127.0.0.1:{}", zk_port);
-    let client = zk::Client::connect(&cluster).await.unwrap();
+    let client = connect(&cluster, chroot).await;
 
     let mut writer = client.new_multi_writer();
     assert_that!(writer.commit().await.unwrap()).is_empty();
