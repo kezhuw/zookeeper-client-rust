274a0a0a8f888d2f7f802c5bcca3a454d6c0811f
Kezhu Wang <kezhuw@gmail.com>
2023-08-06 16:32:49 +0800

    test: enable RUST_LOG env for integration tests

--- tests/zookeeper.rs
+++ tests/zookeeper.rs
@@ -105,7 +105,7 @@ async fn example() {
     assert_eq!(session_event.session_state, zk::SessionState::Closed);
 }
 
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_example() {
     tokio::spawn(async move { example().await }).await.unwrap()
 }
@@ -135,7 +135,7 @@ async fn connect(cluster: &str, chroot: &str) -> zk::Client {
 #[test_case("/"; "no_chroot")]
 #[test_case("/x"; "chroot_x")]
 #[test_case("/x/y"; "chroot_x_y")]
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_multi(chroot: &str) {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
@@ -241,7 +241,7 @@ async fn test_multi(chroot: &str) {
     assert_that!(results).is_empty();
 }
 
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_multi_async_order() {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
@@ -271,7 +271,7 @@ async fn test_multi_async_order() {
     assert_that!(stat).is_equal_to(set_stat);
 }
 
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_no_node() {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
@@ -295,7 +295,7 @@ async fn test_no_node() {
     );
 }
 
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_request_order() {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
@@ -329,7 +329,7 @@ async fn test_request_order() {
     assert_that!(get_child_data.await).is_equal_to(Err(zk::Error::NoNode));
 }
 
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_data_node() {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
@@ -353,7 +353,7 @@ async fn test_data_node() {
     assert_eq!(client.check_stat(path).await.unwrap(), None);
 }
 
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_create_sequential() {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
@@ -376,7 +376,7 @@ async fn test_create_sequential() {
     assert_eq!((data, stat2), client.get_data(&path2).await.unwrap());
 }
 
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_descendants_number() {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
@@ -429,7 +429,7 @@ where
     }
 }
 
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_ephemerals() {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
@@ -495,7 +495,7 @@ async fn test_ephemerals() {
     assert_eq!(vec!["/"], child_root_client.list_ephemerals("/").await.unwrap().into_sorted());
 }
 
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_chroot() {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
@@ -545,7 +545,7 @@ async fn test_chroot() {
     assert_eq!(relative_grandchild_event.path, relative_grandchild_path);
 }
 
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_auth() {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
@@ -571,7 +571,7 @@ async fn test_auth() {
     assert!(authed_users.contains(&authed_user));
 }
 
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_delete() {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
@@ -594,7 +594,7 @@ async fn test_delete() {
     client.delete(path, Some(stat.version)).await.unwrap();
 }
 
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_oneshot_watcher() {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
@@ -771,7 +771,7 @@ async fn test_oneshot_watcher() {
     eprintln!("node deletion done");
 }
 
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_config_watch() {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
@@ -792,7 +792,7 @@ async fn test_config_watch() {
     assert_eq!(event.path, "/zookeeper/config");
 }
 
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_persistent_watcher_passive_remove() {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
@@ -832,7 +832,7 @@ async fn test_persistent_watcher_passive_remove() {
     assert_eq!(child_event.path, "/");
 }
 
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_persistent_watcher() {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
@@ -958,7 +958,7 @@ async fn test_persistent_watcher() {
     assert_eq!(event, path_persistent_watcher.changed().await);
 }
 
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_session_event() {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
@@ -991,7 +991,7 @@ async fn test_session_event() {
     assert_eq!(event, oneshot_watcher4.changed().await);
 }
 
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_state_watcher() {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
@@ -1016,7 +1016,7 @@ async fn test_state_watcher() {
     }
 }
 
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_client_drop() {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
@@ -1032,7 +1032,7 @@ async fn test_client_drop() {
     zk::Client::builder().with_session(id, password).connect(&cluster).await.unwrap_err();
 }
 
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_client_detach() {
     let docker = DockerCli::default();
     let zookeeper = docker.run(zookeeper_image());
@@ -1078,7 +1078,7 @@ admin.enableServer=true";
 /// * https://docs.docker.com/network/drivers/bridge/
 /// * https://docs.docker.com/network/links/
 #[cfg(target_os = "linux")]
-#[tokio::test]
+#[test_log::test(tokio::test)]
 async fn test_update_ensemble() {
     let dir = tempdir().unwrap();
     let docker = DockerCli::default();
