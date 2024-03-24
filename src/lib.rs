29619f1cbf093f6f3fbf02c2cbeeb24c7f52a733
Kezhu Wang <kezhuw@gmail.com>
2024-03-24 09:44:50 +0800

    feat: unsafe option to disable tls hostname verification
    Closes #27.

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,10 @@ mod error;
 mod proto;
 mod record;
 mod session;
+mod tls;
 mod util;
 
 pub use self::acl::{Acl, Acls, AuthId, AuthUser, Permission};
 pub use self::error::Error;
+pub use self::tls::TlsOptions;
 pub use crate::client::*;
