a8cdd4b782cf4da2f6b15d0ed22c2f86a15765f6
Kezhu Wang <kezhuw@gmail.com>
2024-05-07 14:24:34 +0800

    Bump version to 0.8.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+//! `zookeeper-client` is an async Rust client library for Apache ZooKeeper.
+//!
+//! ## Opinionated API
+//! This library is written from scratch. Its API is pretty different from Java counterpart or even
+//! other Rust clients. Some of them are listed here for a glance.
+//!
+//! * No callbacks.
+//! * No catch-all watcher.
+//! * [StateWatcher] tracks session state updates.
+//! * [OneshotWatcher] tracks oneshot ZooKeeper node event.
+//! * [PersistentWatcher] tracks persistent and recursive persistent ZooKeeper node events.
+//! * No event type `XyzWatchRemoved` as Rust has [Drop].
+//! * Most data operations are ordered at future creation time but not polling time.
+//! * [Client::chroot] and [Client::clone] enable session sharing cross multiple different rooted clients.
+//!
+//! ## Feature flags
+//! * `tls`: Toggle TLS support.
+//! * `sasl`: Toggle SASL support.
+//! * `sasl-gssapi`: Toggle only GSSAPI SASL support. This relies on binding package `libgssapi-sys`.
+//! * `sasl-digest-md5`: Toggle only DIGEST-MD5 SASL support.
+
 mod acl;
 mod chroot;
 mod client;
