bdcb80015e9130e3a4dc237a825105a77fddd286
Kezhu Wang <kezhuw@gmail.com>
2024-10-10 08:41:37 +0800

    Doc async runtime support

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,14 @@
 //! * `sasl`: Toggle SASL support.
 //! * `sasl-gssapi`: Toggle only GSSAPI SASL support. This relies on binding package `libgssapi-sys`.
 //! * `sasl-digest-md5`: Toggle only DIGEST-MD5 SASL support.
+//!
+//! ## Async runtime support
+//! This library uses [asyncs](https://docs.rs/asyncs) and [spawns](https://docs.rs/spawns) to
+//! spawn asynchronous tasks. It exposes runtime feature flags for handy. Please refer their docs
+//! for references.
+//! * `tokio`: Toggle support for [tokio](https://docs.rs/tokio).
+//! * `smol`: Toggle support for [smol](https://docs.rs/smol) builtin global executor.
+//! * `async-global-executor`: Toggle support for [async-global-executor](https://docs.rs/async-global-executor).
 
 mod acl;
 mod chroot;
